9df60e7b42ff692f9f1ba05f0664dabf51746a69
AZ <anji.zhu.science@gmail.com>
2026-09-20 00:56:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/angie-s-first-test-of-github/',
   plugins: [react(), tailwindcss()],
 })
